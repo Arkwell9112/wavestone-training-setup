3fe13f0c58ebd1ab326edd2c2b68199a673db750
Edouard Bergé <edouard.berge.reims@gmail.com>
2024-07-25 09:55:09 +0200

    fix

--- awx-check.sh
+++ awx-check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+path="${PWD}"
+
 cd ansible || exit
 
-ansible-playbook awx-check-playbook.yml -i public-inventory-gcp.yml
+ansible-playbook awx-check-playbook.yml -i public-inventory-gcp.yml --extra-vars "path=${path}"
